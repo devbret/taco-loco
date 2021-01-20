c072881dbe8b46f03f030d7d2aace3425cb7b933
Bret Bernhoft <73839896+devbret@users.noreply.github.com>
2021-01-20 06:05:15 -0800

    Improved commentary within and efficiency of code.

--- main.js
+++ main.js
@@ -1,11 +1,10 @@
 function main() {
-    //////////Main Program Variables
+    //////////Program Element Variables
     const interfaceFrame = document.querySelector(`#interfaceFrame`);
     const box = document.createElement(`div`);
     //////////Program State Variables
     //Maintains (or keeps track of) the active elements.
     let active = {divs:[],buttons:[],paragraphs:[],fields:[]};
-    let allDivs = 0;
     let allParas = 0;
     let allButtons = 0;
     let paraClasses = 0;
@@ -14,7 +13,6 @@ function main() {
     function clear() {
         box.innerHTML = ``;
         active = {divs:[],buttons:[],paragraphs:[],fields:[]};
-        allDivs = 0;
         allParas = 0;
         allButtons = 0;
         paraClasses = 0;
@@ -62,7 +60,7 @@ function main() {
         elem.innerHTML = `${text}`;
         box.appendChild(elem);
     }
-    //Creates the "Add New Delivery" button.
+    //Creates a new button.
     function createButton(inrTxt) {
         const elem = document.createElement(`button`);
         elem.classList.add(`buttons`);
@@ -92,96 +90,78 @@ function main() {
     //////////Interface Functions
     //Interface Zero; the first interface shown when the program is loaded.
     function interfaceZero() {
-        //Clearing contents from previous interface.
+        /////Clearing contents from previous interface.
         clear();
-        //Creates an object in local storage.
+        /////Creates an object in local storage.
         createMaster();
-        //Creating the elements.
+        /////Creating the elements.
         createBox();
         createH2(`What Would You Like To Do?`);
         createDiv(`typical`);
         createButton(`Add New Delivery`);
         createButton(`View All Deliveries`);
-        //Assembling the elements.
+        /////Assembling the elements.
         active.divs[0].e.appendChild(active.buttons[0].e);
         active.divs[0].e.appendChild(active.buttons[1].e);
-        //Assigning event listeners to the relevant elements.
+        /////Assigning event listeners to the relevant elements.
         active.buttons[0].e.addEventListener(`click`, interfaceOne);
         active.buttons[1].e.addEventListener(`click`, interfaceTwo);
     }
-    //Interface One; the interface that is created when the "Add New" button is selected.
+    //Interface One; the interface that is created when the "Add New Delivery" button is selected.
     function interfaceOne() {
-        //Clearing contents from previous interface.
+        /////Clearing contents from previous interface.
         clear();
-        //Creating the elements.
+        /////Creating and assembling the elements.
         createH2(`Add New Delivery`);
-        createDiv(`typical`);
-        createPara(`First Name: `);
-        createField(`Enter first name here`);
-        createDiv(`typical`);
-        createPara(`Last Name: `);
-        createField(`Enter last name here`);
-        createDiv(`typical`);
-        createPara(`Address: `);
-        createField(`Enter street address here`);
-        createDiv(`typical`);
-        createPara(`Apt/PMB: `);
-        createField(`Enter apartment or PMB number here`);
-        createDiv(`typical`);
-        createPara(`City: `);
-        createField(`Enter city here`);
-        createDiv(`typical`);
-        createPara(`State: `);
-        createField(`Enter state here`);
-        createDiv(`typical`);
-        createPara(`Zip Code: `);
-        createField(`Enter zip code here`);
+        //Contains the text to be used by the appropriate paragraph and field elements.
+        const paras = [`First Name: `,`Last Name: `,`Address: `,`Apt/PMB: `,`City: `,`State: `,`Zip Code: `];
+        const flds = [`Enter first name here`,`Enter last name here`,`Enter street address here`,`Enter apartment or PMB number here`,`Enter city here`,`Enter state here`,`Enter zip code here`]
+        //Loops through two arrays above, creating and populating elements with the correct text content.
+        for (let i = 0; i < paras.length; i++) {
+            createDiv(`typical`);
+            createPara(paras[i]);
+            createField(flds[i]);
+            active.divs[i].e.appendChild(active.paragraphs[i].e);
+            active.divs[i].e.appendChild(active.fields[i].e);
+        }
+        //Creating both of the "Add New" and "Previous Screen" buttons.
         createDiv(`typical`);
         createButton(`Add New`);
         createButton(`Previous Screen`);
-        //Assembling the elements.
-        active.divs[0].e.appendChild(active.paragraphs[0].e);
-        active.divs[0].e.appendChild(active.fields[0].e);
-        active.divs[1].e.appendChild(active.paragraphs[1].e);
-        active.divs[1].e.appendChild(active.fields[1].e);
-        active.divs[2].e.appendChild(active.paragraphs[2].e);
-        active.divs[2].e.appendChild(active.fields[2].e);
-        active.divs[3].e.appendChild(active.paragraphs[3].e);
-        active.divs[3].e.appendChild(active.fields[3].e);
-        active.divs[4].e.appendChild(active.paragraphs[4].e);
-        active.divs[4].e.appendChild(active.fields[4].e);
-        active.divs[5].e.appendChild(active.paragraphs[5].e);
-        active.divs[5].e.appendChild(active.fields[5].e);
-        active.divs[6].e.appendChild(active.paragraphs[6].e);
-        active.divs[6].e.appendChild(active.fields[6].e);
         active.divs[7].e.appendChild(active.buttons[0].e);
         active.divs[7].e.appendChild(active.buttons[1].e);
-        //Assigning event listeners to the relevant elements.
+        //Assigning event listeners to the relevant button elements.
         active.buttons[0].e.addEventListener(`click`, addNew);
         active.buttons[1].e.addEventListener(`click`, interfaceZero);
     }
     //Interface Two; allows the user to view all of the current deliveries.
     function interfaceTwo() {
-        //Clearing contents from previous interface.
+        /////Clearing contents from previous interface.
         clear();
-        //Accessing the "master" objecting local storage.
+        /////Accessing the "master" objecting local storage.
         const retrievedMaster = localStorage.getItem(`master`);
         const retrievedMasterParsed = JSON.parse(retrievedMaster);
-        //Creating and assembling the elements.
+        /////Creating and assembling the elements.
         let currentSave;
         createH2(`Current Deliveries`);
         for (let i = 0; i < retrievedMasterParsed.length; i++) {
+            //Captures the current paraClasses value, for reference on a per-loop basis.
             currentSave = paraClasses;
             createDiv(`atypical`);
+            //Loops through each item housed in the local storage object.
             for (let z = 0; z < retrievedMasterParsed[i].length; z++) {
+                //Creates the needed paragraph elements, and populates each with the respective data from local storage.
                 createPara(`${(retrievedMasterParsed[i][z] === ``) ? `N/A` : retrievedMasterParsed[i][z]}`,true);
                 active.paragraphs[allParas].e.classList.add(`c${paraClasses}`);
                 active.divs[i].e.appendChild(active.paragraphs[allParas++].e);
             }
+            //Creates the "Save Changes" and "Delete" buttons for each delivery entry.
             createButton(`Save Changes`);
             createButton(`Delete`);
+            //Attaches an event listener to the "Save Changes" button.
             active.divs[i].e.appendChild(active.buttons[allButtons].e)
             active.buttons[allButtons++].e.addEventListener(`click`, function() {
+                //Captures all of the paragraph elements within the appropriate delivery entry, and pushes the content to a temporary array; which then replaces the saved contents inside local storage and saves the data.
                 const pElems = document.querySelectorAll(`.c${currentSave}`);
                 let obj = [];
                 for (let q = 0; q < pElems.length; q++) {
@@ -191,7 +171,9 @@ function main() {
                 localStorage.setItem("master", JSON.stringify(retrievedMasterParsed));
             });
             active.divs[i].e.appendChild(active.buttons[allButtons].e);
+            //Attaches an event listener to the "Delete" button.
             active.buttons[allButtons++].e.addEventListener(`click`, function() {
+                //Creates a new "master" object for local storage, and excludes/removes the appropriate delivery entry from the interface stored object.
                 let newMaster = [];
                 for (let i = 0; i < retrievedMasterParsed.length; i++) {
                     if (i !== currentSave) {
@@ -201,14 +183,12 @@ function main() {
                 localStorage.setItem("master", JSON.stringify(newMaster));
                 active.buttons[currentSave].e.parentNode.parentNode.removeChild(this.parentNode);
             });
+            //Keeps tracks of which class of paragraphs is being accessed.
             paraClasses++;
         }
+        /////Adds the "Previous Screen" button and attaches an event listener to it that redraws the opening interface.
         createButton(`Previous Screen`);
         active.buttons[allButtons].e.addEventListener(`click`, interfaceZero);
-    }
-    //Calling Interface Two through an external function.
-    function callingInterfaceTwo() {
-        interfaceTwo();
     }
     //Creating the first screen.
     interfaceZero();
